be337be58b1ff61e895693a12ee94ad5c4f4e23a
Nick Sedillos <nick.sedillos@gmail.com>
2018-09-27 10:43:08 -0500

    Updated SQL templates.

--- schema-template.sql
+++ schema-template.sql
@@ -4,5 +4,7 @@ CREATE DATABASE new_db_name;
 USE new_db_name;
 
 CREATE TABLE table_name(
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	column_a VARCHAR(255),
+	column_b INT
 )
---
+++ seeds-template.sql
@@ -0,0 +1,4 @@
+USE new_db_name;
+
+INSERT INTO table_name (column_a, column_b)
+VALUES ("value_a1", 1), ("value_a2", 2), ("value_a3", 3);
